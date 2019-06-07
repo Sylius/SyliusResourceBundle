1851697469248207bd698b9e6b59768cf78db2c1
Kamil Kokot <kamil@kokot.me>
2019-06-07 19:27:28 +0800

    Add support for Gedmo DoctrineExtensions

--- src/Bundle/Doctrine/ResourceMappingDriverChain.php
+++ src/Bundle/Doctrine/ResourceMappingDriverChain.php
@@ -15,37 +15,31 @@ namespace Sylius\Bundle\ResourceBundle\Doctrine;
 
 use Doctrine\Common\Persistence\Mapping\ClassMetadata;
 use Doctrine\Common\Persistence\Mapping\Driver\MappingDriver;
+use Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain;
 use Sylius\Component\Resource\Metadata\RegistryInterface;
 
-final class ResourceMappingDriver implements MappingDriver
+/**
+ * It needs to extend MappingDriverChain in order to be compatible with Gedmo/DoctrineExtensions.
+ *
+ * @see \Gedmo\Mapping\ExtensionMetadataFactory::getDriver()
+ */
+final class ResourceMappingDriverChain extends MappingDriverChain
 {
-    /** @var MappingDriver */
-    private $mappingDriver;
-
     /** @var RegistryInterface */
     private $resourceRegistry;
 
     public function __construct(MappingDriver $mappingDriver, RegistryInterface $resourceRegistry)
     {
-        $this->mappingDriver = $mappingDriver;
         $this->resourceRegistry = $resourceRegistry;
+
+        $this->setDefaultDriver($mappingDriver);
     }
 
     public function loadMetadataForClass($className, ClassMetadata $metadata): void
     {
-        $this->mappingDriver->loadMetadataForClass($className, $metadata);
+        parent::loadMetadataForClass($className, $metadata);
 
         $this->convertResourceMappedSuperclass($metadata);
-    }
-
-    public function getAllClassNames(): iterable
-    {
-        return $this->mappingDriver->getAllClassNames();
-    }
-
-    public function isTransient($className): bool
-    {
-        return $this->mappingDriver->isTransient($className);
     }
 
     private function convertResourceMappedSuperclass(ClassMetadata $metadata): void
