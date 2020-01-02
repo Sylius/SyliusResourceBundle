a43600ea9f2f6bd64c0159bcd62038aad359e472
Kamil Kokot <kamil@kokot.me>
2020-01-02 11:57:12 +0100

    Allow for Winzou/StateMachineBundle ^0.4

--- src/Bundle/DependencyInjection/Compiler/WinzouStateMachinePass.php
+++ src/Bundle/DependencyInjection/Compiler/WinzouStateMachinePass.php
@@ -13,14 +13,19 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ResourceBundle\DependencyInjection\Compiler;
 
+use SM\Callback\CallbackFactoryInterface;
+use SM\Callback\CascadeTransitionCallback;
+use SM\Factory\FactoryInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 
 /**
  * Marks WinzouStateMachineBundle's services as public for compatibility with both Symfony 3.4 and 4.0+.
+ * Aliases FQCN-based services for backwards compatibility of Winzou/StateMachineBundle 0.4 with 0.3
  *
  * @see https://github.com/winzou/StateMachineBundle/pull/44
+ * @see https://github.com/winzou/StateMachineBundle/commit/f515c9302783ef2575570d33b20aefa1eb265afb
  */
 final class WinzouStateMachinePass implements CompilerPassInterface
 {
@@ -29,6 +34,10 @@ final class WinzouStateMachinePass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container): void
     {
+        $container->setAlias('sm.factory', FactoryInterface::class);
+        $container->setAlias('sm.callback_factory', CallbackFactoryInterface::class);
+        $container->setAlias('sm.callback.cascade_transition', CascadeTransitionCallback::class);
+
         foreach (['sm.factory', 'sm.callback_factory', 'sm.callback.cascade_transition'] as $id) {
             try {
                 $container->findDefinition($id)->setPublic(true);
