2531d312d35891ed6d18fb38c55fa82bb6beb6db
Loïc Frémont <lc.fremont@gmail.com>
2020-10-16 15:03:27 +0200

    [WIP] Remove twig bundle dependency

--- src/Bundle/test/config/bundles.php
+++ src/Bundle/test/config/bundles.php
@@ -16,7 +16,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sylius\Bundle\ResourceBundle\SyliusResourceBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true, 'test_without_twig' => false],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     AppBundle\AppBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['test' => true],
@@ -24,5 +24,5 @@ return [
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['test' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['test' => true],
-    winzou\Bundle\StateMachineBundle\winzouStateMachineBundle::class => ['test' => true, 'test_without_fosrest' => true],
+    winzou\Bundle\StateMachineBundle\winzouStateMachineBundle::class => ['all' => true, 'test_without_state_machine' => false],
 ];
