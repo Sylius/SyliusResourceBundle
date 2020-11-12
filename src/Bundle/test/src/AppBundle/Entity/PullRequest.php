786065c892c77cea9706f6e7657253125c4f3a6b
Loïc Frémont <lc.fremont@gmail.com>
2020-11-12 16:52:38 +0100

    Fix coding standard

--- src/Bundle/test/src/AppBundle/Entity/PullRequest.php
+++ src/Bundle/test/src/AppBundle/Entity/PullRequest.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of sylius-resource.
+ * This file is part of the Sylius package.
  *
- * (c) Mobizel
+ * (c) Paweł Jędrzejewski
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
