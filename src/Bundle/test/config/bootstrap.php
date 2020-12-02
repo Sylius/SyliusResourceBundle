a46daf3425a9161ac15741a5a88064f5cd970d2d
Loïc Frémont <lc.fremont@gmail.com>
2020-12-02 21:22:52 +0100

    Test env by default on test app console.
    Co-authored-by: Kamil Kokot <kamil@kokot.me>

--- src/Bundle/test/config/bootstrap.php
+++ src/Bundle/test/config/bootstrap.php
@@ -27,6 +27,6 @@ if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
     (new Dotenv(true))->loadEnv(dirname(__DIR__) . '/.env');
 }
 
-$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'test';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], \FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
