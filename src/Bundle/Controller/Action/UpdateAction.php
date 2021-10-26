adbe920aec59a0a4ce87cb6785f48db52242b3b0
Mateusz Zalewski <zaleslaw@gmail.com>
2021-10-26 13:10:04 +0200

    Extract UpdateAction

---
+++ src/Bundle/Controller/Action/UpdateAction.php
@@ -0,0 +1,216 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ResourceBundle\Controller\Action;
+
+use Doctrine\Persistence\ObjectManager;
+use FOS\RestBundle\View\View;
+use Sylius\Bundle\ResourceBundle\Controller\AuthorizationCheckerInterface;
+use Sylius\Bundle\ResourceBundle\Controller\EventDispatcherInterface;
+use Sylius\Bundle\ResourceBundle\Controller\FlashHelperInterface;
+use Sylius\Bundle\ResourceBundle\Controller\RedirectHandlerInterface;
+use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
+use Sylius\Bundle\ResourceBundle\Controller\RequestConfigurationFactoryInterface;
+use Sylius\Bundle\ResourceBundle\Controller\ResourceFormFactoryInterface;
+use Sylius\Bundle\ResourceBundle\Controller\ResourceUpdateHandlerInterface;
+use Sylius\Bundle\ResourceBundle\Controller\SingleResourceProviderInterface;
+use Sylius\Bundle\ResourceBundle\Controller\StateMachineInterface;
+use Sylius\Bundle\ResourceBundle\Controller\ViewHandlerInterface;
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
+use Sylius\Component\Resource\Exception\UpdateHandlingException;
+use Sylius\Component\Resource\Metadata\MetadataInterface;
+use Sylius\Component\Resource\Model\ResourceInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Component\Resource\ResourceActions;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Twig\Environment;
+
+class UpdateAction
+{
+    protected MetadataInterface $metadata;
+    protected RequestConfigurationFactoryInterface $requestConfigurationFactory;
+    protected RepositoryInterface $repository;
+    protected ObjectManager $manager;
+    protected SingleResourceProviderInterface $singleResourceProvider;
+    protected ResourceFormFactoryInterface $resourceFormFactory;
+    protected RedirectHandlerInterface $redirectHandler;
+    protected FlashHelperInterface $flashHelper;
+    protected AuthorizationCheckerInterface $authorizationChecker;
+    protected EventDispatcherInterface $eventDispatcher;
+    protected ResourceUpdateHandlerInterface $resourceUpdateHandler;
+    protected Environment $twig;
+    protected ?ViewHandlerInterface $viewHandler;
+    protected ?StateMachineInterface $stateMachine;
+
+    public function __construct(
+        MetadataInterface $metadata,
+        RequestConfigurationFactoryInterface $requestConfigurationFactory,
+        RepositoryInterface $repository,
+        ObjectManager $manager,
+        SingleResourceProviderInterface $singleResourceProvider,
+        ResourceFormFactoryInterface $resourceFormFactory,
+        RedirectHandlerInterface $redirectHandler,
+        FlashHelperInterface $flashHelper,
+        AuthorizationCheckerInterface $authorizationChecker,
+        EventDispatcherInterface $eventDispatcher,
+        ResourceUpdateHandlerInterface $resourceUpdateHandler,
+        Environment $twig,
+        ?ViewHandlerInterface $viewHandler,
+        ?StateMachineInterface $stateMachine
+    ) {
+        $this->metadata = $metadata;
+        $this->requestConfigurationFactory = $requestConfigurationFactory;
+        $this->repository = $repository;
+        $this->manager = $manager;
+        $this->singleResourceProvider = $singleResourceProvider;
+        $this->resourceFormFactory = $resourceFormFactory;
+        $this->redirectHandler = $redirectHandler;
+        $this->flashHelper = $flashHelper;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->resourceUpdateHandler = $resourceUpdateHandler;
+        $this->twig = $twig;
+        $this->viewHandler = $viewHandler;
+        $this->stateMachine = $stateMachine;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+
+        $this->isGrantedOr403($configuration, ResourceActions::UPDATE);
+        $resource = $this->findOr404($configuration);
+
+        $form = $this->resourceFormFactory->create($configuration, $resource);
+        $form->handleRequest($request);
+
+        if (
+            in_array($request->getMethod(), ['POST', 'PUT', 'PATCH'], true)
+            && $form->isSubmitted()
+            && $form->isValid()
+        ) {
+            $resource = $form->getData();
+
+            /** @var ResourceControllerEvent $event */
+            $event = $this->eventDispatcher->dispatchPreEvent(ResourceActions::UPDATE, $configuration, $resource);
+
+            if ($event->isStopped() && !$configuration->isHtmlRequest()) {
+                throw new HttpException($event->getErrorCode(), $event->getMessage());
+            }
+            if ($event->isStopped()) {
+                $this->flashHelper->addFlashFromEvent($configuration, $event);
+
+                $eventResponse = $event->getResponse();
+                if (null !== $eventResponse) {
+                    return $eventResponse;
+                }
+
+                return $this->redirectHandler->redirectToResource($configuration, $resource);
+            }
+
+            try {
+                $this->resourceUpdateHandler->handle($resource, $configuration, $this->manager);
+            } catch (UpdateHandlingException $exception) {
+                if (!$configuration->isHtmlRequest()) {
+                    return $this->createRestView($configuration, $form, $exception->getApiResponseCode());
+                }
+
+                $this->flashHelper->addErrorFlash($configuration, $exception->getFlash());
+
+                return $this->redirectHandler->redirectToReferer($configuration);
+            }
+
+            if ($configuration->isHtmlRequest()) {
+                $this->flashHelper->addSuccessFlash($configuration, ResourceActions::UPDATE, $resource);
+            }
+
+            $postEvent = $this->eventDispatcher->dispatchPostEvent(ResourceActions::UPDATE, $configuration, $resource);
+
+            if (!$configuration->isHtmlRequest()) {
+                if ($configuration->getParameters()->get('return_content', false)) {
+                    return $this->createRestView($configuration, $resource, Response::HTTP_OK);
+                }
+
+                return $this->createRestView($configuration, null, Response::HTTP_NO_CONTENT);
+            }
+
+            $postEventResponse = $postEvent->getResponse();
+            if (null !== $postEventResponse) {
+                return $postEventResponse;
+            }
+
+            return $this->redirectHandler->redirectToResource($configuration, $resource);
+        }
+
+        if (!$configuration->isHtmlRequest()) {
+            return $this->createRestView($configuration, $form, Response::HTTP_BAD_REQUEST);
+        }
+
+        $initializeEvent = $this->eventDispatcher->dispatchInitializeEvent(ResourceActions::UPDATE, $configuration, $resource);
+        $initializeEventResponse = $initializeEvent->getResponse();
+        if (null !== $initializeEventResponse) {
+            return $initializeEventResponse;
+        }
+
+        return new Response($this->twig->render($configuration->getTemplate(ResourceActions::UPDATE . '.html'), [
+            'configuration' => $configuration,
+            'metadata' => $this->metadata,
+            'resource' => $resource,
+            $this->metadata->getName() => $resource,
+            'form' => $form->createView(),
+        ]));
+    }
+
+    /**
+     * @throws AccessDeniedException
+     */
+    protected function isGrantedOr403(RequestConfiguration $configuration, string $permission): void
+    {
+        if (!$configuration->hasPermission()) {
+            return;
+        }
+
+        $permission = $configuration->getPermission($permission);
+
+        if (!$this->authorizationChecker->isGranted($configuration, $permission)) {
+            throw new AccessDeniedException();
+        }
+    }
+
+    /**
+     * @throws NotFoundHttpException
+     */
+    protected function findOr404(RequestConfiguration $configuration): ResourceInterface
+    {
+        if (null === $resource = $this->singleResourceProvider->get($configuration, $this->repository)) {
+            throw new NotFoundHttpException(sprintf('The "%s" has not been found', $this->metadata->getHumanizedName()));
+        }
+
+        return $resource;
+    }
+
+    protected function createRestView(RequestConfiguration $configuration, $data, int $statusCode = null): Response
+    {
+        if (null === $this->viewHandler) {
+            throw new \LogicException('You can not use the "non-html" request if FriendsOfSymfony Rest Bundle is not available. Try running "composer require friendsofsymfony/rest-bundle".');
+        }
+
+        $view = View::create($data, $statusCode);
+
+        return $this->viewHandler->handle($configuration, $view);
+    }
+
+    protected function getStateMachine(): StateMachineInterface
+    {
+        if (null === $this->stateMachine) {
+            throw new \LogicException('You can not use the "state-machine" if Winzou State Machine Bundle is not available. Try running "composer require winzou/state-machine-bundle".');
+        }
+
+        return $this->stateMachine;
+    }
+}
