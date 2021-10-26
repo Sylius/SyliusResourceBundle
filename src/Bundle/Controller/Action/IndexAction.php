bfaf43181eb75a3a3c798e8296112960b969c93a
Mateusz Zalewski <zaleslaw@gmail.com>
2021-10-26 12:57:10 +0200

    Extract IndexAction

---
+++ src/Bundle/Controller/Action/IndexAction.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ResourceBundle\Controller\Action;
+
+use FOS\RestBundle\View\View;
+use Sylius\Bundle\ResourceBundle\Controller\AuthorizationCheckerInterface;
+use Sylius\Bundle\ResourceBundle\Controller\EventDispatcherInterface;
+use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
+use Sylius\Bundle\ResourceBundle\Controller\RequestConfigurationFactoryInterface;
+use Sylius\Bundle\ResourceBundle\Controller\ResourcesCollectionProviderInterface;
+use Sylius\Bundle\ResourceBundle\Controller\ViewHandlerInterface;
+use Sylius\Component\Resource\Metadata\MetadataInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Component\Resource\ResourceActions;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Twig\Environment;
+
+class IndexAction
+{
+    protected MetadataInterface $metadata;
+    protected RequestConfigurationFactoryInterface $requestConfigurationFactory;
+    protected EventDispatcherInterface $eventDispatcher;
+    protected AuthorizationCheckerInterface $authorizationChecker;
+    protected RepositoryInterface $repository;
+    protected Environment $twig;
+    protected ResourcesCollectionProviderInterface $resourcesCollectionProvider;
+    protected ?ViewHandlerInterface $viewHandler;
+
+    public function __construct(
+        MetadataInterface $metadata,
+        RequestConfigurationFactoryInterface $requestConfigurationFactory,
+        EventDispatcherInterface $eventDispatcher,
+        AuthorizationCheckerInterface $authorizationChecker,
+        RepositoryInterface $repository,
+        Environment $twig,
+        ResourcesCollectionProviderInterface $resourcesCollectionProvider,
+        ?ViewHandlerInterface $viewHandler
+    ) {
+        $this->metadata = $metadata;
+        $this->requestConfigurationFactory = $requestConfigurationFactory;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->repository = $repository;
+        $this->twig = $twig;
+        $this->resourcesCollectionProvider = $resourcesCollectionProvider;
+        $this->viewHandler = $viewHandler;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+
+        $this->isGrantedOr403($configuration, ResourceActions::INDEX);
+        $resources = $this->resourcesCollectionProvider->get($configuration, $this->repository);
+
+        $this->eventDispatcher->dispatchMultiple(ResourceActions::INDEX, $configuration, $resources);
+
+        if ($configuration->isHtmlRequest()) {
+            return new Response($this->twig->render($configuration->getTemplate(ResourceActions::INDEX . '.html'), [
+                'configuration' => $configuration,
+                'metadata' => $this->metadata,
+                'resources' => $resources,
+                $this->metadata->getPluralName() => $resources,
+            ]));
+        }
+
+        return $this->createRestView($configuration, $resources);
+    }
+
+    /**
+     * @throws AccessDeniedException
+     */
+    protected function isGrantedOr403(RequestConfiguration $configuration, string $permission): void
+    {
+        if (!$configuration->hasPermission()) {
+            return;
+        }
+
+        $permission = $configuration->getPermission($permission);
+
+        if (!$this->authorizationChecker->isGranted($configuration, $permission)) {
+            throw new AccessDeniedException();
+        }
+    }
+
+    protected function createRestView(RequestConfiguration $configuration, $data, int $statusCode = null): Response
+    {
+        if (null === $this->viewHandler) {
+            throw new \LogicException('You can not use the "non-html" request if FriendsOfSymfony Rest Bundle is not available. Try running "composer require friendsofsymfony/rest-bundle".');
+        }
+
+        $view = View::create($data, $statusCode);
+
+        return $this->viewHandler->handle($configuration, $view);
+    }
+}
