a30f8d57b9034c5b071dd6a2b1ec2bc239e951f9
Loïc Frémont <lc.fremont@gmail.com>
2021-12-20 12:05:04 +0100

    Tring to support Symfony 4.4

--- src/Bundle/Controller/Parameters.php
+++ src/Bundle/Controller/Parameters.php
@@ -14,17 +14,61 @@ declare(strict_types=1);
 namespace Sylius\Bundle\ResourceBundle\Controller;
 
 use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpKernel\Kernel;
 
-class Parameters extends ParameterBag
-{
-    public function get(string $key, $default = null)
+if (Kernel::MAJOR_VERSION === 6) {
+    class Parameters extends ParameterBag
     {
-        $result = parent::get($key, $default);
+        public function get(string $key, mixed $default = null): mixed
+        {
+            $result = parent::get($key, $default);
 
-        if (null === $result && $default !== null && $this->has($key)) {
-            $result = $default;
+            if (null === $result && $default !== null && $this->has($key)) {
+                $result = $default;
+            }
+
+            return $result;
         }
+    }
 
-        return $result;
+} else if (Kernel::MAJOR_VERSION === 5) {
+    class Parameters extends ParameterBag
+    {
+        /**
+         * @param mixed $default
+         *
+         * @return mixed
+         */
+        public function get(string $key, $default = null)
+        {
+            $result = parent::get($key, $default);
+
+            if (null === $result && $default !== null && $this->has($key)) {
+                $result = $default;
+            }
+
+            return $result;
+        }
+    }
+
+} else {
+    class Parameters extends ParameterBag
+    {
+        /**
+         * @param string $key
+         * @param mixed $default
+         *
+         * @return mixed
+         */
+        public function get($key, $default = null)
+        {
+            $result = parent::get($key, $default);
+
+            if (null === $result && $default !== null && $this->has($key)) {
+                $result = $default;
+            }
+
+            return $result;
+        }
     }
 }
