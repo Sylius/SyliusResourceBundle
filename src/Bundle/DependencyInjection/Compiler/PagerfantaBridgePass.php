07d3f5b8eac3387527b0e63894bf6d6d41257137
Loïc Frémont <lc.fremont@gmail.com>
2022-03-28 17:20:05 +0200

    Add support for Symfony 6

--- src/Bundle/DependencyInjection/Compiler/PagerfantaBridgePass.php
+++ src/Bundle/DependencyInjection/Compiler/PagerfantaBridgePass.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ResourceBundle\DependencyInjection\Compiler;
 
+use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -46,14 +47,26 @@ final class PagerfantaBridgePass implements CompilerPassInterface
 
     private function aliasRenamedServices(ContainerBuilder $container): void
     {
+        $setDeprecatedMethod = (new \ReflectionClass(Alias::class))->getMethod('setDeprecated');
+
         if ($container->hasDefinition('pagerfanta.twig_extension')) {
-            $container->setAlias('twig.extension.pagerfanta', 'pagerfanta.twig_extension')
-                ->setDeprecated(true, 'The "%alias_id%" service alias is deprecated since Sylius 1.8, use the "pagerfanta.twig_extension" service ID instead.');
+            if (2 === $setDeprecatedMethod->getNumberOfParameters()) {
+                $container->setAlias('twig.extension.pagerfanta', 'pagerfanta.twig_extension')
+                    ->setDeprecated(true, 'The "%alias_id%" service alias is deprecated since Sylius 1.8, use the "pagerfanta.twig_extension" service ID instead.');
+            } else {
+                $container->setAlias('twig.extension.pagerfanta', 'pagerfanta.twig_extension')
+                    ->setDeprecated('sylius/resource-bundle', '1.8', 'The "%alias_id%" service alias is deprecated since Sylius 1.8, use the "pagerfanta.twig_extension" service ID instead.');
+            }
         }
 
         if ($container->hasDefinition('pagerfanta.view_factory')) {
-            $container->setAlias('white_october_pagerfanta.view_factory', 'pagerfanta.view_factory')
-                ->setDeprecated(true, 'The "%alias_id%" service alias is deprecated since Sylius 1.8, use the "pagerfanta.view_factory" service ID instead.');
+            if (2 === $setDeprecatedMethod->getNumberOfParameters()) {
+                $container->setAlias('white_october_pagerfanta.view_factory', 'pagerfanta.view_factory')
+                    ->setDeprecated(true, 'The "%alias_id%" service alias is deprecated since Sylius 1.8, use the "pagerfanta.view_factory" service ID instead.');
+            } else {
+                $container->setAlias('white_october_pagerfanta.view_factory', 'pagerfanta.view_factory')
+                    ->setDeprecated('sylius/resource-bundle', '1.8', 'The "%alias_id%" service alias is deprecated since Sylius 1.8, use the "pagerfanta.view_factory" service ID instead.');
+            }
         }
     }
 }
