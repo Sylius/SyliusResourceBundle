dc22e5ba76f0b8fd681a02e68f9fdb8c6daf4533
Loïc Frémont <lc.fremont@gmail.com>
2022-04-20 11:13:31 +0200

    Add a note to remove Twig pass on sylius resource bundle 2.0

--- src/Bundle/DependencyInjection/Compiler/TwigPass.php
+++ src/Bundle/DependencyInjection/Compiler/TwigPass.php
@@ -16,6 +16,9 @@ namespace Sylius\Bundle\ResourceBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+/**
+ * TODO Remove on sylius/resource-bundle 2.0
+ */
 final class TwigPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
