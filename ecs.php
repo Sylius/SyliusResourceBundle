d05b01880e8b38955f5cf8cb555e1b74668447a6
Loïc Frémont <lc.fremont@gmail.com>
2022-06-17 14:34:48 +0200

    Upgrade ECS configuration to fix the build

--- ecs.php
+++ ecs.php
@@ -4,15 +4,13 @@ use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use PhpCsFixer\Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer;
 use SlevomatCodingStandard\Sniffs\Commenting\InlineDocCommentDeclarationSniff;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
+return static function (ECSConfig $config): void {
+    $config->import('vendor/sylius-labs/coding-standard/ecs.php');
 
-    $containerConfigurator
-        ->services()
-        ->set(HeaderCommentFixer::class)->call('configure', [[
+    $config
+        ->ruleWithConfiguration(HeaderCommentFixer::class, [
             'location' => 'after_open',
             'header' =>
 'This file is part of the Sylius package.
@@ -21,11 +19,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.',
-        ]])
-        ->set(NoSuperfluousPhpdocTagsFixer::class)->call('configure', [['allow_mixed' => true]])
-    ;
+        ]);
 
-    $containerConfigurator->parameters()->set(Option::SKIP, [
+    $config->ruleWithConfiguration(NoSuperfluousPhpdocTagsFixer::class, ['allow_mixed' => true]);
+
+    $config->skip([
         InlineDocCommentDeclarationSniff::class . '.MissingVariable',
         VisibilityRequiredFixer::class => ['*Spec.php'],
         'src/Bundle/Controller/ControllerTrait.php',
