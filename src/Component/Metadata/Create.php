7a9182490303c8d22e95dea4af5651ecb7b54423
Loïc Frémont <lc.fremont@gmail.com>
2022-12-01 16:27:25 +0100

    Add create operation with minimal data

---
+++ src/Component/Metadata/Create.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Resource\Metadata;
+
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
+/**
+ * @experimental
+ */
+final class Create extends Operation implements CreateOperationInterface
+{
+    public function __construct(
+        ?string $name = null,
+        ?array $methods = null,
+        ?string $path = null,
+        ?string $routePrefix = null,
+        ?string $controller = null,
+        ?string $template = null,
+    ) {
+        parent::__construct(
+            name: $name ?? 'create',
+            methods: $methods ?? ['GET', 'POST'],
+            path: $path,
+            routePrefix: $routePrefix,
+            controller: $controller,
+            template: $template,
+        );
+    }
+}
