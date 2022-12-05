6f5793828f7e428a055ae34905ba8fe00f534349
Loïc Frémont <lc.fremont@gmail.com>
2022-12-05 23:41:18 +0100

    Improve context

--- src/Component/Context/Context.php
+++ src/Component/Context/Context.php
@@ -18,21 +18,31 @@ namespace Sylius\Component\Resource\Context;
  */
 final class Context implements \IteratorAggregate
 {
-    /** @var array<class-string<object>, object> */
+    /** @var array<class-string, object> */
     private array $optionMap;
 
-    public function __construct(object ...$options)
+    /**
+     * @param object|array<class-string, object> ...$options
+     */
+    public function __construct(object|array ...$options)
     {
         $map = [];
         foreach ($options as $option) {
+            if (\is_array($option)) {
+                $map = array_merge($map, $option);
+
+                continue;
+            }
+
             $map[get_class($option)] = $option;
         }
 
         $this->optionMap = $map;
     }
 
-    public function with(object ...$options): self
+    public function with(object|array ...$options): self
     {
+        /** @psalm-suppress DuplicateArrayKey */
         return new self(...[
             ...array_values($this->optionMap),
             ...$options,
