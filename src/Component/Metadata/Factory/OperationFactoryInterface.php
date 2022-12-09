6a0710fdf03ed95e8b653252395b3ad4fe477ffb
Loïc Frémont <lc.fremont@gmail.com>
2022-12-09 17:54:17 +0100

    Add operation factory

---
+++ src/Component/Metadata/Factory/OperationFactoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Resource\Metadata\Factory;
+
+use Sylius\Component\Resource\Metadata\Operation;
+
+interface OperationFactoryInterface
+{
+    /**
+     * @psalm-param class-string $operationClass
+     */
+    public function create(string $operationClass, array $options): Operation;
+}
