860b5eaac6f5bfddac88bb184a7f4b1d640e84b6
Loïc Frémont <lc.fremont@gmail.com>
2023-01-17 10:58:40 +0100

    Add http operation initiator

---
+++ src/Component/Metadata/Operation/HttpOperationInitiator.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Resource\Metadata\Operation;
+
+use Sylius\Component\Resource\Metadata\HttpOperation;
+use Sylius\Component\Resource\Metadata\RegistryInterface;
+use Sylius\Component\Resource\Metadata\Resource\Factory\ResourceMetadataCollectionFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+final class HttpOperationInitiator
+{
+    public function __construct(
+        private RegistryInterface $resourceRegistry,
+        private ResourceMetadataCollectionFactoryInterface $resourceMetadataCollectionFactory,
+    ) {
+    }
+
+    public function initializeOperation(Request $request): ?HttpOperation
+    {
+        if (
+            [] === ($attributes = $request->attributes->all('_sylius')) ||
+            null === ($resource = $attributes['resource'] ?? null) ||
+            null === ($operationName = $request->attributes->get('_route'))
+        ) {
+            return null;
+        }
+
+        if (str_contains($resource, '.')) {
+            $metadata = $this->resourceRegistry->get($resource);
+        } else {
+            $metadata = $this->resourceRegistry->getByClass($resource);
+        }
+
+        /** @var HttpOperation $operation */
+        $operation = $this->resourceMetadataCollectionFactory->create($metadata->getClass('model'))
+            ->getOperation($metadata->getAlias(), $operationName)
+        ;
+
+        return $operation;
+    }
+}
