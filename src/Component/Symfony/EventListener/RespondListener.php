b88af7a4c141b42d25e72aa44e3071c2b912dd35
Loïc Frémont <lc.fremont@gmail.com>
2023-01-23 18:41:16 +0100

    Add respond listener

---
+++ src/Component/Symfony/EventListener/RespondListener.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Resource\Symfony\EventListener;
+
+use Sylius\Component\Resource\Context\Initiator\RequestContextInitiator;
+use Sylius\Component\Resource\Metadata\Operation\HttpOperationInitiator;
+use Sylius\Component\Resource\State\ResponderInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Webmozart\Assert\Assert;
+
+final class RespondListener
+{
+    public function __construct(
+        private HttpOperationInitiator $operationInitiator,
+        private RequestContextInitiator $contextInitiator,
+        private ResponderInterface $responder,
+    ) {
+    }
+
+    public function onKernelView(ViewEvent $event): void
+    {
+        $request = $event->getRequest();
+        $context = $this->contextInitiator->initializeContext($request);
+        $operation = $this->operationInitiator->initializeOperation($request);
+
+        $controllerResult = $event->getControllerResult();
+
+        if (
+            null === $operation ||
+            $controllerResult instanceof Response
+        ) {
+            return;
+        }
+
+        $response = $this->responder->respond($controllerResult, $operation, $context);
+        Assert::isInstanceOf($response, Response::class);
+
+        $event->setResponse($response);
+    }
+}
