74278986e6e05ab1abdb0d1c5ef12757ad3d6501
Loïc Frémont <lc.fremont@gmail.com>
2023-02-04 09:42:16 +0100

    Add default provider

---
+++ src/Component/Reflection/Filter/FunctionArgumentsFilter.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Resource\Reflection\Filter;
+
+final class FunctionArgumentsFilter
+{
+    public static function filter(\ReflectionFunctionAbstract $reflectionFunction, array $arguments): array
+    {
+        $repositoryArguments = self::getFunctionArguments($reflectionFunction);
+
+        $allowed = array_intersect_key($repositoryArguments, $arguments);
+
+        return array_intersect_key($arguments, array_flip(array_keys($allowed)));
+    }
+
+    private static function getFunctionArguments(\ReflectionFunctionAbstract $reflectionFunction): array
+    {
+        $arguments = [];
+
+        foreach ($reflectionFunction->getParameters() as $param) {
+            $arguments[$param->name] = null;
+        }
+
+        return $arguments;
+    }
+}
