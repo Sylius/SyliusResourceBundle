313cdd2ccb6b41411c7b0d048fa625d88d812a6d
Loïc Frémont <lc.fremont@gmail.com>
2023-02-04 14:50:39 +0100

    Add Doctrine processors

---
+++ src/Component/Doctrine/Common/Metadata/Resource/Factory/DoctrineResourceMetadataCollectionFactory.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Resource\Doctrine\Common\Metadata\Resource\Factory;
+
+use Sylius\Component\Resource\Doctrine\Common\State\PersistProcessor;
+use Sylius\Component\Resource\Doctrine\Common\State\RemoveProcessor;
+use Sylius\Component\Resource\Metadata\DeleteOperationInterface;
+use Sylius\Component\Resource\Metadata\Operation;
+use Sylius\Component\Resource\Metadata\Operations;
+use Sylius\Component\Resource\Metadata\RegistryInterface;
+use Sylius\Component\Resource\Metadata\Resource as ResourceMetadata;
+use Sylius\Component\Resource\Metadata\Resource\Factory\ResourceMetadataCollectionFactoryInterface;
+use Sylius\Component\Resource\Metadata\Resource\ResourceMetadataCollection;
+
+class DoctrineResourceMetadataCollectionFactory implements ResourceMetadataCollectionFactoryInterface
+{
+    public function __construct(
+        private RegistryInterface $resourceRegistry,
+        private ResourceMetadataCollectionFactoryInterface $decorated,
+    ) {
+    }
+
+    public function create(string $resourceClass): ResourceMetadataCollection
+    {
+        $resourceCollectionMetadata = $this->decorated->create($resourceClass);
+
+        /** @var ResourceMetadata $resource */
+        foreach ($resourceCollectionMetadata->getIterator() as $i => $resource) {
+            $operations = $resource->getOperations() ?? new Operations();
+
+            /** @var Operation $operation */
+            foreach ($operations as $operation) {
+                /** @var string $key */
+                $key = $operation->getName();
+
+                $operations->add($key, $this->addDefaults($resource, $operation));
+            }
+
+            $resource = $resource->withOperations($operations);
+
+            $resourceCollectionMetadata[$i] = $resource;
+        }
+
+        return $resourceCollectionMetadata;
+    }
+
+    private function addDefaults(ResourceMetadata $resource, Operation $operation): Operation
+    {
+        $metadata = $this->resourceRegistry->get($resource->getAlias());
+
+        if (str_starts_with($metadata->getDriver(), 'doctrine/')) {
+            $operation = $operation->withProcessor($this->getProcessor($operation));
+        }
+
+        return $operation;
+    }
+
+    private function getProcessor(Operation $operation): callable|string
+    {
+        if (null !== $processor = $operation->getProcessor()) {
+            return $processor;
+        }
+
+        if ($operation instanceof DeleteOperationInterface) {
+            return RemoveProcessor::class;
+        }
+
+        return PersistProcessor::class;
+    }
+}
