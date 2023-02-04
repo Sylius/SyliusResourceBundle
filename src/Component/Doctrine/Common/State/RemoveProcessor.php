9b2b02b444e1ef543d36bb10a2d9176763fc7d9b
Loïc Frémont <lc.fremont@gmail.com>
2023-02-04 14:50:39 +0100

    Add Doctrine processors

---
+++ src/Component/Doctrine/Common/State/RemoveProcessor.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Resource\Doctrine\Common\State;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager as DoctrineObjectManager;
+use Sylius\Component\Resource\Context\Context;
+use Sylius\Component\Resource\Metadata\Operation;
+use Sylius\Component\Resource\Reflection\ClassInfoTrait;
+use Sylius\Component\Resource\State\ProcessorInterface;
+
+final class RemoveProcessor implements ProcessorInterface
+{
+    use ClassInfoTrait;
+
+    public function __construct(private ManagerRegistry $managerRegistry)
+    {
+    }
+
+    public function process(mixed $data, Operation $operation, Context $context): mixed
+    {
+        if (!\is_object($data) || !$manager = $this->getManager($data)) {
+            return null;
+        }
+
+        $manager->remove($data);
+        $manager->flush();
+
+        return null;
+    }
+
+    /**
+     * Gets the Doctrine object manager associated with given data.
+     */
+    private function getManager(object $data): ?DoctrineObjectManager
+    {
+        return $this->managerRegistry->getManagerForClass($this->getObjectClass($data));
+    }
+}
