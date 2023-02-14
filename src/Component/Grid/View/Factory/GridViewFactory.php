06c751b667ac7a82fb2fd1368ab3a28ce4ac2ddf
Loïc Frémont <lc.fremont@gmail.com>
2023-02-14 13:54:04 +0100

    Add Grid provider

---
+++ src/Component/Grid/View/Factory/GridViewFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Resource\Grid\View\Factory;
+
+use Sylius\Component\Grid\Data\DataProviderInterface;
+use Sylius\Component\Grid\Definition\Grid;
+use Sylius\Component\Grid\Parameters;
+use Sylius\Component\Grid\View\GridView;
+
+final class GridViewFactory implements GridViewFactoryInterface
+{
+    public function __construct(
+        private DataProviderInterface $dataProvider,
+    ) {
+    }
+
+    public function create(Grid $grid, Parameters $parameters, array $driverConfiguration): GridView
+    {
+        return new GridView($this->dataProvider->getData($grid, $parameters), $grid, $parameters);
+    }
+}
