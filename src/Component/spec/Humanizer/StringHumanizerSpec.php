5dab48e0ebd1c3623bc76660107872f110a16804
Loïc Frémont <lc.fremont@gmail.com>
2023-02-21 12:37:00 +0100

    Add Flash helper & listener

---
+++ src/Component/spec/Humanizer/StringHumanizerSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\Sylius\Component\Resource\Humanizer;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Resource\Humanizer\StringHumanizer;
+
+final class StringHumanizerSpec extends ObjectBehavior
+{
+    function it_is_initializable(): void
+    {
+        $this->shouldHaveType(StringHumanizer::class);
+    }
+
+    function it_humanizes_a_string(): void
+    {
+        $this::humanize('admin_user')->shouldReturn('admin user');
+        $this::humanize('Admin_user')->shouldReturn('admin user');
+        $this::humanize('AdminUser')->shouldReturn('admin user');
+    }
+}
