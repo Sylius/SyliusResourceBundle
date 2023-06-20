0478696cd0b891bbdbc8441915f2d765575f03db
Loïc Frémont <lc.fremont@gmail.com>
2023-06-20 14:36:43 +0200

    Fix coding standard

--- tests/Application/tmp/Sylius/State/BookItemProvider.php
+++ tests/Application/tmp/Sylius/State/BookItemProvider.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace App\Tests\Tmp\Sylius\State;
 
 use Sylius\Component\Resource\Context\Context;
