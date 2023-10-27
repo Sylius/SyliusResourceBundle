29fbe649f3c246bf8d2435ae1c667402589af893
Rafikooo <thecrazyrafik@gmail.com>
2023-10-27 20:20:30 +0200

    Add generic typehint to the TranslatableFactoryInterface

--- src/Component/Factory/TranslatableFactoryInterface.php
+++ src/Component/Factory/TranslatableFactoryInterface.php
@@ -13,6 +13,9 @@ declare(strict_types=1);
 
 namespace Sylius\Component\Resource\Factory;
 
+/**
+ * @template T of object
+ */
 interface TranslatableFactoryInterface extends FactoryInterface
 {
 }
