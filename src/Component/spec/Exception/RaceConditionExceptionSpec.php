3d784f40ac501df5b3eed5948f7854d9e4adea7d
Loïc Frémont <lc.fremont@gmail.com>
2024-03-28 09:07:43 +0100

    Move spec file

--- src/Component/spec/Exception/RaceConditionExceptionSpec.php
+++ src/Component/spec/Exception/RaceConditionExceptionSpec.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace spec\Sylius\Component\Resource\Exception;
+namespace spec\Sylius\Resource\Exception;
 
 use PhpSpec\ObjectBehavior;
 use Sylius\Resource\Exception\UpdateHandlingException;
