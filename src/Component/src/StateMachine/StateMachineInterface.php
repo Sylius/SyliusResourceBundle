da0a99c2e3ba43958f07387dd087d1e9a306cc25
Loïc Frémont <lc.fremont@gmail.com>
2024-04-03 14:08:31 +0200

    Apply suggestions from code review
    Co-authored-by: Dmitri Perunov <diimpp@gmail.com>

--- src/Component/src/StateMachine/StateMachineInterface.php
+++ src/Component/src/StateMachine/StateMachineInterface.php
@@ -18,14 +18,12 @@ use SM\StateMachine\StateMachineInterface as BaseStateMachineInterface;
 interface StateMachineInterface extends BaseStateMachineInterface
 {
     /**
-     * Returns the possible transition from given state
-     * Returns null if no transition is possible
+     * Returns the possible transition from given state or null if no transition is possible
      */
     public function getTransitionFromState(string $fromState): ?string;
 
     /**
-     * Returns the possible transition to the given state
-     * Returns null if no transition is possible
+     * Returns the possible transition to the given state or null if no transition is possible
      */
     public function getTransitionToState(string $toState): ?string;
 }
