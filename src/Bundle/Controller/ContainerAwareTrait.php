1a1ee520f9e7a8c2294321606768218c2c7ad1c6
Loïc Frémont <lc.fremont@gmail.com>
2024-04-22 14:40:42 +0200

    Copy container aware trait to keep using ResourceController

---
+++ src/Bundle/Controller/ContainerAwareTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ResourceBundle\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Copied from Symfony to keep using ResourceController as this trait has been removed in Symfony 7.
+ * Do not use this trait on your projects, use dependency injection instead.
+ *
+ * @see https://github.com/symfony/symfony/blob/6.4/src/Symfony/Component/DependencyInjection/ContainerAwareTrait.php
+ *
+ * @internal
+ */
+trait ContainerAwareTrait
+{
+    protected ?ContainerInterface $container = null;
+
+    public function setContainer(?ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+}
