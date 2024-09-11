2ea2de058caaabb41b776a36ef3d2e9a156dd0fa
Loïc Frémont <lc.fremont@gmail.com>
2024-09-11 21:49:15 +0200

    Move tests

--- src/Component/tests/Reflection/CallableReflectionTest.php
+++ src/Component/tests/Reflection/CallableReflectionTest.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace Sylius\Resource\Tests\Reflection;
 
 use PHPUnit\Framework\TestCase;
-use ReflectionFunctionAbstract;
 use Sylius\Component\Resource\Tests\Dummy\RepositoryWithCallables;
 use Sylius\Resource\Reflection\CallableReflection;
 
@@ -29,24 +28,24 @@ final class CallableReflectionTest extends TestCase
     public function testItReflectsAnArrayCallable(): void
     {
         $reflection = CallableReflection::from([RepositoryWithCallables::class, 'find']);
-        $this->assertInstanceOf(ReflectionFunctionAbstract::class, $reflection);
+        $this->assertInstanceOf(\ReflectionFunctionAbstract::class, $reflection);
     }
 
     public function testItReflectsAClosureCallable(): void
     {
         $reflection = CallableReflection::from(fn (): array => []);
-        $this->assertInstanceOf(ReflectionFunctionAbstract::class, $reflection);
+        $this->assertInstanceOf(\ReflectionFunctionAbstract::class, $reflection);
     }
 
     public function testItReflectsAStringCallable(): void
     {
         $reflection = CallableReflection::from('Sylius\Component\Resource\Tests\Dummy\RepositoryWithCallables::find');
-        $this->assertInstanceOf(ReflectionFunctionAbstract::class, $reflection);
+        $this->assertInstanceOf(\ReflectionFunctionAbstract::class, $reflection);
     }
 
     public function testItReflectsAnInvokableCallable(): void
     {
         $reflection = CallableReflection::from(new RepositoryWithCallables());
-        $this->assertInstanceOf(ReflectionFunctionAbstract::class, $reflection);
+        $this->assertInstanceOf(\ReflectionFunctionAbstract::class, $reflection);
     }
 }
