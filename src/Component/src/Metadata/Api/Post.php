adc137c8364076c7a4db846853b52ee244a97fae
Loïc Frémont <lc.fremont@gmail.com>
2025-02-27 12:10:50 +0100

    Add route requirements

--- src/Component/src/Metadata/Api/Post.php
+++ src/Component/src/Metadata/Api/Post.php
@@ -25,6 +25,7 @@ final class Post extends HttpOperation implements CreateOperationInterface, ApiO
     public function __construct(
         ?string $path = null,
         ?string $routePrefix = null,
+        ?array $routeRequirements = null,
         ?string $template = null,
         ?string $shortName = null,
         ?string $name = null,
@@ -51,6 +52,7 @@ final class Post extends HttpOperation implements CreateOperationInterface, ApiO
             methods: ['POST'],
             path: $path,
             routePrefix: $routePrefix,
+            routeRequirements: $routeRequirements,
             template: $template,
             shortName: $shortName ?? 'post',
             name: $name,
