fc96543d504fe05043488f1df9b68e16845580c6
Grzegorz Sadowski <sadowskigp@gmail.com>
2025-06-02 09:47:23 +0200

    Fix implicit nullable parameter + add ECS rule

--- src/Component/src/Exception/StorageUnavailableException.php
+++ src/Component/src/Exception/StorageUnavailableException.php
@@ -15,7 +15,7 @@ namespace Sylius\Resource\Exception;
 
 class StorageUnavailableException extends RuntimeException
 {
-    public function __construct(string $message = '', \Exception $previous = null)
+    public function __construct(string $message = '', ?\Exception $previous = null)
     {
         parent::__construct($message, 0, $previous);
     }
