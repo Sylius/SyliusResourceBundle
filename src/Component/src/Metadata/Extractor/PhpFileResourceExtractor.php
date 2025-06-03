abf47700dd19c0318da12e18fafea5c2de1355d4
Loïc Frémont <lc.fremont@gmail.com>
2025-06-03 21:10:35 +0200

    Fix extractor with multiple files

--- src/Component/src/Metadata/Extractor/PhpFileResourceExtractor.php
+++ src/Component/src/Metadata/Extractor/PhpFileResourceExtractor.php
@@ -33,7 +33,7 @@ final class PhpFileResourceExtractor extends AbstractResourceExtractor
             $property->setValue($resource, $resolvedValue);
         }
 
-        $this->resources = [$resource];
+        $this->resources[] = $resource;
     }
 
     /**
